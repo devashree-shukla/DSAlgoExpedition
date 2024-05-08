1d893ab8d2a4211c49bdf26c3d0119fcd4403b4a
Devashree Shukla <shukladevashree963@gmail.com>
2024-05-08 15:49:44 +0530

    Stack - trapping rain water problem

--- Leetcoding/Stack/TrappingRainWater.playground/Contents.swift
+++ Leetcoding/Stack/TrappingRainWater.playground/Contents.swift
@@ -1,14 +1,33 @@
 
 func trap(_ height: [Int]) -> Int {
+//    var water = 0
+//            var left = 0
+//            var right = height.count - 1
+//            var leftMax = height[left]
+//            var rightMax = height[right]
+//
+//            while left < right{
+//                if leftMax < rightMax{
+//                    left += 1
+//                    leftMax = max(leftMax, height[left])
+//                    water += leftMax - height[left]
+//                } else {
+//                    right -= 1
+//                    rightMax = max(rightMax, height[right])
+//                    water += rightMax - height[right]
+//                }
+//            }
+//            return water
+    guard height.count > 1 else { return 0 }
     var stack: [Int] = []
     var result = 0
+    
     for i in 0 ..< height.count {
-        while !stack.isEmpty, height[stack.last!] <= height[i] {
-            let stacktop = stack.popLast()
+        while !stack.isEmpty, height[stack.last!] < height[i] {
+            let stacktop = stack.removeLast()
             if stack.count > 0 {
-                let h = Swift.min(height[stack.last!], height[i] - height[stacktop!])
-                let w = i - (stack.last! + 1)
-                result += h * w
+                let h = Swift.min(height[i], height[stack.last!] - height[stacktop])
+                result += (i - stack.last! - 1) * h
             }
         }
         stack.append(i)
